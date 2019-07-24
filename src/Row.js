6258e2b4838d1d014a05280602626dfbb25095e9
Jim Renwick <jim.renwick@webgp.com>
2019-07-24 15:46:48 +0100

    made it nicer looking and added picture and link icons

--- src/Row.js
+++ src/Row.js
@@ -3,26 +3,34 @@ import {getSuggestionStream} from "./Observe";
 
 function Row(props) {
 
-    const [name, setName] = useState("LOADING...");
+    const [info, setInfo] = useState({"login":"LOADING..."});
 
     const rowId = "closeBtn" + props.btnId;
 
     useEffect(() => {
 
-        var thing = getSuggestionStream(rowId)
+        var subStream = getSuggestionStream(rowId)
             .subscribe(json => {
-                setName(json.login)
+                // console.log(json);
+                setInfo(json)
             });
 
         // returned function will be called when the component unmounts
-        return () => thing.unsubscribe();
+        return () => subStream.unsubscribe();
 
         // an input of empty array means useEffect will only be called once
     }, [rowId]);
 
     return (
-        <li>{name}<a href="#" id={rowId} className="close">x</a>
+        <li>
+            <div>
+                <img src={info.avatar_url}/>
+                <a href={info.html_url} target="_blank" className="username">{info.login}</a>
+            </div>
+            {/*even though the IDE doesn't like this, React knows to look in the public folder for the image*/}
+            <img id={rowId} src="/closeIcon.png"/>
         </li>
+
     )
 }
 
