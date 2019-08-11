d0128ab16972252f4a4dea99c22dd95381e658f4
Jim Renwick <jim.renwick@webgp.com>
2019-08-11 22:57:07 +0100

    cleaned up CSS and added more info to the cards

--- src/App.js
+++ src/App.js
@@ -9,24 +9,24 @@ function App() {
     return (
         <div className="container">
             <div className="bird-container bird-container--one">
-                <div className="bird bird--one"></div>
+                <div className="bird bird--one"/>
             </div>
             <div className="bird-container bird-container--two">
-                <div className="bird bird--two"></div>
+                <div className="bird bird--two"/>
             </div>
 
             <div className="bird-container bird-container--three">
-                <div className="bird bird--three"></div>
+                <div className="bird bird--three"/>
             </div>
 
             <div className="bird-container bird-container--four">
-                <div className="bird bird--four"></div>
+                <div className="bird bird--four"/>
             </div>
             <div className="logo">
                 <img alt="can't find logo" src="/logo.png"/>
             </div>
             <SearchBox/>
-            <MyAlert/>
+            {/*<MyAlert/>*/}
             <br/>
             <ResultsScroll/>
         </div>
--- src/ResultCard.js
+++ src/ResultCard.js
@@ -1,19 +1,44 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import React from "react";
+import React, {useState} from "react";
+import Collapse from "react-bootstrap/Collapse";
 
 export default function ResultCard(props) {
 
+    const [open, setOpen] = useState(false);
+
     return (
-        <Card key={props.index} style={{"fontFamily": "Impact, Charcoal, sans-serif"}}>
-        <Card.Header as="h5">{props.data.name}</Card.Header>
-        <Card.Body>
-            <Card.Title>{props.data.junkName}</Card.Title>
-            <Card.Text>
-                {props.data.description}
-            </Card.Text>
-            <Button variant="outline-primary">See more</Button>
-        </Card.Body>
-    </Card>
+        <Card key={props.index}>
+            <Card.Header>
+                <div style={{"display": "flex", "alignItems":"center"}}>
+                    <div style={{"flex": "1", "fontFamily": "Impact, Charcoal, sans-serif"}}>{props.data.name.toUpperCase()}</div>
+                    <div><b>Tickets Left:</b> {props.data.tickets}</div>
+                </div>
+
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{props.data.description}</Card.Title>
+                <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                    in culpa qui officia deserunt mollit anim id est laborum.
+                </Card.Text>
+                <Button onClick={() => setOpen(!open)}
+                        aria-expanded={open}
+                        variant="outline-info">Details</Button>
+                <Collapse in={open}>
+                    <div>
+                        <br/>
+                        <div>
+                            <p><b>Junk:</b> {props.data.junkName}</p>
+                            <p><b>Departure Time:</b> {new Date(props.data.departureDttm).toLocaleTimeString("uk")}</p>
+                        </div>
+                    </div>
+                </Collapse>
+
+            </Card.Body>
+        </Card>
     )
 }
