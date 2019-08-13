7ab503e115481e7a3adad9e98d075ecc46db1291
Jim Renwick <jim.renwick@webgp.com>
2019-08-13 11:19:38 +0100

    updated droppers to allow All to be selected as an option

--- src/Dropper.js
+++ src/Dropper.js
@@ -4,14 +4,20 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 export function Dropper(props) {
 
+    const DEFAULT_EVENT_KEY="NONE_SELECTED";
     let [value, setValue] = useState("");
 
     const placeholder = props.placeholder;
 
     function handleOptionClick(eventKey, e) {
         let text = e.target.text;
-        setValue(text);
-        props.streamSubject.next(eventKey);
+        if (eventKey === DEFAULT_EVENT_KEY) {
+            setValue(null);
+            props.streamSubject.next(null);
+        } else {
+            setValue(text);
+            props.streamSubject.next(eventKey);
+        }
     }
 
     return (
@@ -20,6 +26,8 @@ export function Dropper(props) {
                 as={props => <FormControl id={props.id} placeholder={placeholder} {...props} value={value}/>}
                 autoComplete="off"/>
             <Dropdown.Menu>
+                <Dropdown.Item key={DEFAULT_EVENT_KEY} eventKey={DEFAULT_EVENT_KEY} onSelect={handleOptionClick}>All</Dropdown.Item>
+                <Dropdown.Divider />
                 {props.optionsList.map(((val, key) => <Dropdown.Item key={key} eventKey={val[0]} onSelect={handleOptionClick}>{val[1]}</Dropdown.Item>))}
             </Dropdown.Menu>
         </Dropdown>
